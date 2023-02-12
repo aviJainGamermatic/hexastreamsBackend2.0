a106e33f0b4948d4e63fd6ff3341e6ea4cf54a90
Rahul Banjare <rahulbanjare@Rahuls-MacBook-Air.local>
2023-02-12 20:24:08 +0530

    added cors plugin

--- src/app/app.js
+++ src/app/app.js
@@ -8,10 +8,12 @@ app.use('/user', require('./routes/userRoutes'));
 app.use("/game",require('./routes/gameRoutes') )
 dbConfig.dbConnect();
 
+app.use(cors());
 app.listen(port, () => {
   console.log(`GamerMatic app listening on port ${port}`);
 });
-
+app.use(function ())
 app.get("/", (req, res) => {
+
   res.send("Wlcome to gamermatic backed Service!!");
 });
