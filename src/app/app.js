dbf24900c25ec3af1f536fd5be4231ad083463d0
Rahul Banjare <rahulbanjare@Rahuls-MacBook-Air.local>
2023-02-13 11:20:12 +0530

    added cors to be allowed from all origins

--- src/app/app.js
+++ src/app/app.js
@@ -4,12 +4,17 @@ const cors = require('cors');
 const dbConfig = require('./dbConfig');
 require('dotenv').config();
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.use(express.json());
 app.use('/user', require('./routes/userRoutes'));
 app.use("/game",require('./routes/gameRoutes') )
 dbConfig.dbConnect();
 
-app.use(cors());
+
 app.listen(port, () => {
   console.log(`GamerMatic app listening on port ${port}`);
 });
@@ -17,3 +22,6 @@ app.get("/", (req, res) => {
 
   res.send("Wlcome to gamermatic backed Service!!");
 });
+
+
+
