27b4369462cf2aeeb98dda9f8577779492c8c658
aviJainGamermatic <fullstack02@gamermatic.in>
2023-06-10 00:11:49 +0530

    .

--- src/app/routes/userRoutes.js
+++ src/app/routes/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = new express.Router();
 const userController = require('../controller/userController');
+const savelinkedAcc=require('../controller/linkedAcc')
 
 router.post('/signUp', userController.signUp);
 router.post('/register', userController.register);
 router.post('/verify', userController.verify);
 router.post('/login', userController.login);
 router.post('/update', userController.Update);
+router.post('/users/:userId/accounts',savelinkedAcc.save);
 
 
-
-module.exports=router;
+w
