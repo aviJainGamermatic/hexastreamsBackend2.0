763b2ffa22c1c83a95e68120590ff7071f6a4b08
ArpanGamermatic <132878048+ArpanGamermatic@users.noreply.github.com>
2023-06-20 22:44:03 +0530

    Update youtubeRoutes.js

--- src/app/routes/youtubeRoutes.js
+++ src/app/routes/youtubeRoutes.js
@@ -3,22 +3,34 @@ const express = require('express');
 const router = new express.Router();
 const youtubeController = require("../controller/Youtubecontroller");
 const verify = require("../middleware/auth")
+ require("../middleware/passport-setup");
+ const passport=require('passport');
+const app=express();
 
 
+router.get('/google',passport.authenticate('google',{scope:['profile','email']}));
 
 //creating authentication url
 router.get('/generateUrl',youtubeController. signUpwithGoogle);
 
-//redirect url
-router.get("/auth/callback",youtubeController.redirectUri);
+//to authenitcate google
+router.get('/auth/callback', passport.authenticate('google', { failureRedirect: '/google' }), (req, res) => {
+    // Redirect to the desired page after successful authentication
+    res.redirect('/google-auth');
+  });
 
-//google aith
-router.get("/google-auth",youtubeController.googleauth);
+
+  //updating user model
+  router.get("/google-auth",youtubeController.googleauth);
+
+//redirect url and creating linked account
+router.get("/auth/callback2",youtubeController.redirectUri);
+
 
 //lonking google account
-router.get("/googleaccounts",verify,youtubeController.googleaccounts);
+//router.get("/googleaccounts",verify,youtubeController.googleaccounts);
 
 //generating strean key
-router.get("/generateyoutubestreamkey",verify,youtubeController.streamKey);
+router.post("/generateyoutubestreamkey",youtubeController.streamKey);
 
 module.exports=router;
