30266aeea0a9bc75bf82b88eba5926176e013955
Rahul Banjare <rahulbanjare@Rahuls-MacBook-Air.local>
2023-06-21 21:52:18 +0530

    fixed

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
-
-const port = 5000;
+const passport= require ('passport');
+const session = require('express-session');
+const port = 5001;
 const cors = require('cors');
 const dbConfig = require('./dbConfig');
 require('dotenv').config();
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
+app.use(session({
+  secret: 'your_session_secret',
+  resave: false,
+  saveUninitialized: false,
+}));
 app.use(express.json());
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/user', require('./routes/userRoutes'));
 app.use("/game",require('./routes/gameRoutes') );
 app.use("/live-stream",require("./routes/liveStreamingRoutes"));
-app.use("/",require("./routes/youtubeRoutes"));
+app.use("/youtube",require("./routes/youtubeRoutes"));
 app.use("/razorpay",require("./routes/razorpayroutes"));
 app.use("/twitch",require("./routes/twitchRoutes"));
 dbConfig.dbConnect();
